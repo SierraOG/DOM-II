154d415f800e89ef6d1ed422d21077c330c30bb7
SierraOG <sierrao@utexas.edu>
2019-06-18 15:22:09 -0500

    added popup form and click and submit events

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+// Navbar event
 navlist = document.querySelectorAll('nav a');
 
 for (let i = 0; i < navlist.length; i++) {             
@@ -13,7 +15,7 @@ for (let i = 0; i < navlist.length; i++) {
   });
 }
 
-
+// Fun bus image event
 let scale = 1;
 function zoom(event) {
     event.preventDefault();
@@ -27,5 +29,31 @@ function zoom(event) {
     letsGo.style.transform = `scale(${scale})`;
 }
   
-letsGo = document.querySelector('.intro img');
+let letsGo = document.querySelector('.intro img');
 letsGo.addEventListener("wheel", zoom);
+
+// Add pop up form
+const popForm = document.querySelector(".form-popup");
+
+const buttons = document.querySelectorAll('.destination .btn');
+buttons[0].addEventListener('click', (event) => {
+  popForm.style.display = 'block';
+})
+buttons[1].addEventListener('click', (event) => {
+  popForm.style.display = 'block';
+})
+buttons[2].addEventListener('click', (event) => {
+  popForm.style.display = 'block';
+})
+
+let closebtn = document.querySelector('.form-container .btn2.cancel');
+closebtn.addEventListener('click', function(){
+  popForm.style.display = 'none';
+});
+
+function logSubmit(event) {
+  alert(`Form Submitted! Time stamp: ${event.timeStamp}`);
+  popForm.style.display = 'none';
+}
+
+popForm.addEventListener('submit', logSubmit);
